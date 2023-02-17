1061822c2ea903d0913850d3c21fa41332cdf30c
saji37 <sajiththomas162631@gmail.com>
2023-02-17 22:58:29 +0530

    Update Welcome.js

--- src/Welcome.js
+++ src/Welcome.js
@@ -7,11 +7,7 @@ function Welcome() {
     <motion.div
     initial={{ x: "-50px", opacity: 10 }}
     animate={{ x: 10, opacity: 20 }}
-<<<<<<< Updated upstream
-    transition={{type:"waves",delay:1,duration:1}}
-=======
     transition={{type:"waves",delay:1,duration:3}}
->>>>>>> Stashed changes
     // exit={{ y: '-1000px', opacity: 0 }}
   >
     <div className='greeting'>
@@ -21,15 +17,9 @@ function Welcome() {
     </motion.div>
 
 <motion.div
-<<<<<<< Updated upstream
-initial={{ y: "1000px", opacity: 10 }}
-animate={{ y: 10, opacity: 20 }}
-transition={{type:"waves",delay:3,duration:1}}
-=======
 initial={{ x: "10px", opacity: 10 }}
 animate={{ x: '-20px',y:'-30px', opacity: 20 }}
 transition={{type:"waves",delay:3}}
->>>>>>> Stashed changes
 
 // exit={{ y: '-1000px', opacity: 0 }}
 >
