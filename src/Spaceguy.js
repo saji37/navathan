453091d553a19c614bb950af0c27ce42091f2bb8
saji37 <sajiththomas162631@gmail.com>
2023-02-17 19:02:12 +0530

    spaceguy

---
+++ src/Spaceguy.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import  './Spaceguy.css'
+function Spaceguy() {
+  return (
+  
+  <div class="astronaut" data-js="astro">
+    <div class="head"></div>
+    <div class="arm arm-left"></div>
+    <div class="arm arm-right"></div>
+    <div class="body">
+      <div class="panel"></div>
+    </div>
+    <div class="leg leg-left"></div>
+    <div class="leg leg-right"></div>
+    <div class="schoolbag"></div>
+  </div>
+  )
+}
+
+export default Spaceguy
