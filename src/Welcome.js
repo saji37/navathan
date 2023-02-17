53a3ea0f88ca5b74b2cb6ddd04e361cf63942395
saji37 <sajiththomas162631@gmail.com>
2023-02-17 21:37:28 +0530

    chnges

--- src/Welcome.js
+++ src/Welcome.js
@@ -5,21 +5,31 @@ function Welcome() {
   return (
     <div>
     <motion.div
-    initial={{ x: "-1000px", opacity: 10 }}
+    initial={{ x: "-50px", opacity: 10 }}
     animate={{ x: 10, opacity: 20 }}
+<<<<<<< Updated upstream
     transition={{type:"waves",delay:1,duration:1}}
+=======
+    transition={{type:"waves",delay:1,duration:3}}
+>>>>>>> Stashed changes
     // exit={{ y: '-1000px', opacity: 0 }}
   >
     <div className='greeting'>
-     <center><h1 className='welcome'>WELCOME TO </h1></center> 
+     <center><h1 style={{fontSize:'40px'}}>WELCOME TO </h1></center> 
 
     </div>
     </motion.div>
 
 <motion.div
+<<<<<<< Updated upstream
 initial={{ y: "1000px", opacity: 10 }}
 animate={{ y: 10, opacity: 20 }}
 transition={{type:"waves",delay:3,duration:1}}
+=======
+initial={{ x: "10px", opacity: 10 }}
+animate={{ x: '-20px',y:'-30px', opacity: 20 }}
+transition={{type:"waves",delay:3}}
+>>>>>>> Stashed changes
 
 // exit={{ y: '-1000px', opacity: 0 }}
 >
