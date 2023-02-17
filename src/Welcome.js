9a98ad57b3e5ef4cc3d46d4b7db306f01b843aeb
GeethanjaliKS <geethanjaliks001@gmail.com>
2023-02-17 20:11:28 +0530

    welcome

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,11 +1,34 @@
 import React from 'react'
-
+import { motion } from 'framer-motion'
+import './Welcome.css'
 function Welcome() {
   return (
-    <div className='App'>
-      <h1>WELCOME TO NAVATHAN</h1>
+    <div>
+    <motion.div
+    initial={{ x: "-1000px", opacity: 0 }}
+    animate={{ x: 0, opacity: 10 }}
+    transition={{type:"spring"}}
+    // exit={{ y: '-1000px', opacity: 0 }}
+  >
+    <div className='greeting'>
+      <h1 className='welcome'>WELCOME TO </h1>
 
     </div>
+    </motion.div>
+
+<motion.div
+initial={{ x: "1000px", opacity: 0 }}
+animate={{ x: 0, opacity: 10 }}
+transition={{type:"spring"}}
+onDurationChange={2}
+// exit={{ y: '-1000px', opacity: 0 }}
+>
+<div className='greeting'>
+  <h1 className=''> NAVATHAN</h1>
+
+</div>
+</motion.div>
+</div>
   )
 }
 
